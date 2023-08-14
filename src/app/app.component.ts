cb39aca2465268c15638c9e672fae82414f64597
salsabeel <salsabeelhatem52@yahoo.com>
2023-08-14 08:50:58 +0300

    added json file for books

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
+import books from '../assets/books.json';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'MyBookStore';
+  data: any = books;
+
+  ngOnInit() {
+    console.log('Data', this.data);
+  }
 }
