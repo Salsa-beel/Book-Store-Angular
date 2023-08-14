a95b9e7207c0cf516a267d01e545bb428f1f7d46
salsabeel <salsabeelhatem52@yahoo.com>
2023-08-14 09:44:12 +0300

    rearranging the project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component,OnInit } from '@angular/core';
-import books from '../assets/books.json';
+import* as books from '../assets/books.json';
 
 @Component({
   selector: 'app-root',
@@ -9,6 +9,7 @@ import books from '../assets/books.json';
 export class AppComponent implements OnInit{
   title = 'MyBookStore';
   data: any = books;
+  author: any;
 
   ngOnInit() {
     console.log('Data', this.data);
