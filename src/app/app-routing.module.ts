b99122e0e47e10b776e1d7bf1261c40c05fb36c5
salsabeel <salsabeelhatem52@yahoo.com>
2023-08-16 16:01:11 +0300

    added breaking news components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { BooksModule } from './books/books.module';
 import { DetailsofbooksComponent } from './books/components/detailsofbooks/detailsofbooks.component';
 import { ListofbooksComponent } from './books/components/listofbooks/listofbooks.component';
+import { BreakingNewsComponent } from './breaking-news/breaking-news.component';
 
 const routes: Routes = [
   {path:"books",component:ListofbooksComponent},
-  {path:"details",component:DetailsofbooksComponent},
-  {path:"**",redirectTo:"products",pathMatch:"full"}
+  {path:"details/:id",component:DetailsofbooksComponent},
+  {path:"breakingNews",component:BreakingNewsComponent},
+  {path:"**",redirectTo:"breakingNews",pathMatch:"full"}
 ];
 
 @NgModule({
