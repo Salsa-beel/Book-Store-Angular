919821d23c4e0e4c214fafa077e6fc650f3439e3
salsabeel <123001325+Salsa-beel@users.noreply.github.com>
2023-08-20 11:07:08 +0300

    deleted entity and class as i didn't use it

--- src/app/entities/book.entity.ts
+++
@@ -1,9 +0,0 @@
-export class Book{
-
-    id: number | any;
-    title: string | any;
-    author:string | any;
-    publicationYear!: number;
-    description!: string ;
-    thumbnailUrl!: string;
-} 
